f6dc283d1473d5827590189918d1d46e2bf13e6f
Vsevolod <vsevolod.gordienko99@gmail.com>
2019-12-21 14:29:29 +0300

    d

--- DigitalAge/Back-End/DigitalAge/DigitalAge/Models/ProductRepository.cs
+++ DigitalAge/Back-End/DigitalAge/DigitalAge/Models/ProductRepository.cs
@@ -28,16 +28,34 @@ namespace DigitalAge.Models
                 Category = "Computers",
                 Price = 1487,
                 Description = "домашний/геймерский, CPU Intel Core i5 9400F 2900 МГц, RAM DDR4 16 ГБ, SSD 480 ГБ, NVIDIA GeForce GTX 1660 6 ГБ, БП 500 Вт",
-                Image = "https://fk.by/uploads/images/cache/computers/kompyuter-bez-monitora-amd-a4_1-1100x500.jpg"
+                Image = "https://content2.onliner.by/catalog/device/header/7cd37e4850f8812fc737d4d811eddca2.jpeg"
             });
             Add(new Product
             {
-                Id = 3,
+                Id = 4,
                 Name = "Irwin Computers Matisse G1-17",
                 Category = "Computers",
                 Price = 1690,
-                Description = "None",
-                Image = "https://fk.by/uploads/images/cache/computers/kompyuter-bez-monitora-amd-a4_1-1100x500.jpg"
+                Description = "15.6 1920 x 1080 IPS, AMD Ryzen 5 3550H 2100 МГц, 8 ГБ, SSD 512 ГБ, граф. адаптер: AMD Radeon RX 560X 4 ГБ, без ОС, цвет крышки черный",
+                Image = "https://content2.onliner.by/catalog/device/header/09b7968a445ac1c2ed2c8b7c289719c0.jpeg"
+            });
+            Add(new Product
+            {
+                Id = 5,
+                Name = "ASUS TUF Gaming FX505DY-BQ024",
+                Category = "Laptops",
+                Price = 1490,
+                Description = "домашний/геймерский, CPU Intel Core i5 9400F 2900 МГц, RAM DDR4 16 ГБ, SSD 480 ГБ, NVIDIA GeForce GTX 1660 6 ГБ, БП 500 Вт",
+                Image = "https://content2.onliner.by/catalog/device/header/8b98d73d89e7118f4a91a487b24e342e.jpeg"
+            });
+            Add(new Product
+            {
+                Id = 6,
+                Name = "Lenovo IdeaPad S340-15API 81NC00F0RE",
+                Category = "Laptops",
+                Price = 1045,
+                Description = "1920 x 1080 IPS, Intel Core i9 9980HK 2400 МГц, 64 ГБ, HDD+SSD 2000+2048 ГБ, граф. адаптер: NVIDIA GeForce RTX 2080 8 ГБ, Windows 10, цвет крышки черный",
+                Image = "https://content2.onliner.by/catalog/device/header/b368d51af7af1cc2039985e9e07ce431.jpeg"
             });
         }
 
